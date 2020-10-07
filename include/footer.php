591cbd388e02a1e57e3b2e1f8ce859a3417d4133
Jeewantha Bandara <51124088+JeewanthaBandara97@users.noreply.github.com>
2020-10-07 12:21:11 +0530

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,12 @@
+
+
+<div  class="card mb-3 grey bg-dark text-white fixed">
+  <div class="card-body">
+    <p class="card-text">	
+		  <div class="footer-copyright text-center py-3">© 2020 Copyright :
+			<a href="https://#.com/"> Sigiri Groups</a>
+		  </div>
+    <p class="card-text"><small class="text-muted"></small></p>
+  </div>
+</div>
+
